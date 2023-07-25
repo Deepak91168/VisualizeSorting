5e04ac6d8190d97ab6d0d173f6ed9a8cbd1e514e
Jacob Castanon <jacastanon01@gmail.com>
2023-07-25 15:10:41 -0500

    initial solution

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,26 @@
 const d = new Date();
+// let timer = document.getElementById("timer").innerHTML
 const startTime = d.getTime();
+let size = Number(document.querySelector(".size_drop").value);
 function Timer(starttime) {
-  setInterval(() => {
-    var start_time = startTime;
+  // let timer =
+  // setInterval(() => {
+  // document.getElementById("timer").innerHTML
+    console.log("timer running")
+
+    var start_time = starttime;
     var current_time = new Date().getTime();
     var difference = current_time - start_time;
     var minutes = Math.floor((difference % (60 * 60 * 1000)) / (1000 * 60));
     var seconds = Math.floor((difference % (60 * 1000)) / 1000);
     var milliseconds = Math.floor(difference % 1000);
-    document.getElementById("timer").innerHTML =
-      "Time: " + minutes + "m " + seconds + "s " + milliseconds + "ms";
-  }, 1);
+    document.getElementById("timer").innerHTML = "Time: " + minutes + "m " + seconds + "s " + milliseconds + "ms";
+  // }, 1000);
+
+  // if (isDone) {
+  //   clearInterval(timer)
+  // }
+  
 }
 function randomNumber(low, high) {
   var create_randomNum = parseInt(
@@ -49,23 +59,39 @@ function count_swaps(property) {
     "Swaps: " + property);
   return counts;
 }
+
+function stopTime(inter){
+  document.getElementById("timer").innerHTML = "Time: 00: 00: 00"
+  return clearInterval(inter)
+}
 const start = async () => {
   let algo = Number(document.querySelector(".algo_drop").value);
   let speed = Number(document.querySelector(".speed_drop").value);
   let algorithm = new Algorithm(speed);
+  let interval = setInterval(() => Timer(startTime), 100);
   await Alert_for_speed(speed);
   await Alert_for_Algo(algo);
-  Timer(startTime);
+  // Problem may be invoking th eTimer function i this async function.
   if (algo === 1) await algorithm.BubbleSort();
   if (algo === 2) await algorithm.InsertionSort();
   // if (algo === 3) await algorithm.shelleSort();
   if (algo === 4) await algorithm.SelectionSort();
   if (algo === 5) await algorithm.MergeSort();
   if (algo === 6) await algorithm.QuickSort();
-  var stopTime = document.getElementById("timer").innerHTML;
-  document.getElementById("timer").remove();
-  document.getElementById("showTime").innerHTML = stopTime;
-  document.getElementById("showTime").setAttribute("style", "display:flex");
+  //const stopTime = document.getElementById("timer").innerHTML;
+  //document.getElementById("timer").remove();
+  // document.getElementById("showTime").innerHTML = "Time: 00: 00: 00"
+  // document.getElementById("showTime").setAttribute("style", "display:flex");
+  let doneCells = document.querySelectorAll(".cell.done")
+  console.log(Array.from(doneCells).length, size)
+  const isDone = Array.from(doneCells).length === size
+  if (isDone) {
+    console.log("FINISHED")
+    interval = clearInterval(interval)
+    // stopTime(interval)
+    // return () => clearInterval(interval)
+  }
+  return clearInterval(interval)
 };
 
 const RenderScreen = async () => {
@@ -103,7 +129,7 @@ const RenderList = async () => {
 };
 
 const RenderArray = async (sorted) => {
-  let size = Number(document.querySelector(".size_drop").value);
+
   await clear();
   let list = await randomList(size);
   if (sorted) list.sort((a, b) => a - b);
